0681d426e84d7bb44e3bf758df97b6bd24ac26ae
Scott McIntire <scottmcintire90@gmail.com>
2016-04-22 06:54:17 -0700

    add front end code to templates

--- src/main/java/VowelRemover.java
+++ src/main/java/VowelRemover.java
@@ -1,29 +1,29 @@
 import java.util.*;
 import java.util.Map;
 import java.util.HashMap;
-// import spark.ModelAndView;
-// import spark.template.velocity.VelocityTemplateEngine;
-// import static spark.Spark.*;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
 
 public class VowelRemover {
   public static void main(String[] args) {
-    // get("/", (request, response) -> {
-    //   Map<String, Object> model = new HashMap<String, Object>();
-    //   model.put("template", "templates/home.vtl");
-    //   return new ModelAndView(model, "templates/layout.vtl");
-    // }, new VelocityTemplateEngine());
-    //
-    // get("/detector", (request, response) -> {
-    //   Map<String, Object> model = new HashMap<String, Object>();
-    //
-    //   String userInput = request.queryParams("number");
-    //   NumbersToWords newNumbers = new NumbersToWords();
-    //   String convertedNumber = newNumbers.integerConverter(userInput);
-    //   model.put("convertedNumber", convertedNumber);
-    //
-    //   model.put("template", "templates/detector.vtl");
-    //   return new ModelAndView(model, "templates/layout.vtl");
-    // }, new VelocityTemplateEngine());
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/home.vtl");
+      return new ModelAndView(model, "templates/layout.vtl");
+    }, new VelocityTemplateEngine());
+
+    get("/results", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+
+      String userInput = request.queryParams("userInput");
+      VowelRemover newInput = new VowelRemover();
+      String vowelsRemoved = newInput.vowelRemover(userInput);
+      model.put("vowelsRemoved", vowelsRemoved);
+
+      model.put("template", "templates/results.vtl");
+      return new ModelAndView(model, "templates/layout.vtl");
+    }, new VelocityTemplateEngine());
   }
 
   public static String vowelRemover(String sentence) {
--- src/test/java/VowelRemoverTest.java
+++ src/test/java/VowelRemoverTest.java
@@ -21,4 +21,5 @@ public class VowelRemoverTest {
     String expected = "- -m - c-t";
     assertEquals(expected, testVowelRemover.vowelRemover("I am a cat"));
   }
+  
 }
